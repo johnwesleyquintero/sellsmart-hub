574753ca68a3d7a580481ade78a13f522b132c84
John Wesley Quintero <johnwesleyquintero@gmail.com>
2025-03-26 09:10:12 +0800

    style: fix indentation in external-links.ts

--- lib/config/external-links.ts
+++ lib/config/external-links.ts
@@ -1,5 +1,5 @@
 export const externalLinks = [
-    { name: "SS Docs", href: "https://sellsmart-docs.vercel.app/" },
+  { name: "SS Docs", href: "https://sellsmart-docs.vercel.app/" },
   { name: "SS Hub", href: "https://sellsmart-hub.vercel.app/" },
   { name: "SS Pro - BETA", href: "https://sellsmart-pro.vercel.app/" },
 ];
