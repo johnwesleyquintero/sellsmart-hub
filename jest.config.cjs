b4dd88e49757ec54c768720f9bddf0e29cbd3997
John Wesley Quintero <johnwesleyquintero@gmail.com>
2025-04-28 20:58:34 +0800

    ci(jest): update Jest configuration and package dependencies
    Updated Jest configuration to use @swc/jest for faster TypeScript and JSX transformations. Added @swc/core and @swc/jest as dependencies. Modified the reset script to include build and start commands for a complete environment reset.

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,26 @@
 module.exports = {
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+            decorators: true,
+          },
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
+  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  cacheDirectory: '.jest/cache',
 };
